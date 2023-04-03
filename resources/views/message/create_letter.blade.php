403fd301d4fb32c04a14de0b0b051b1a62aa53bc
TaisukeMiyachi <taisuke.m.lotus.elise@gmail.com>
2023-04-03 23:48:20 +0900

    reverse

--- resources/views/message/create_letter.blade.php
+++ resources/views/message/create_letter.blade.php
@@ -28,7 +28,7 @@
     <!-- ヘッダー -->
     <nav class="w-full bg-white shadow-lg">
         <div class="flex items-center  h-24 max-w-7xl mx-auto px-4 sm:px-6 lg:px-">
-            <div id="header-left" class="w-1/3 flex start flex items-center text-gray-500 hover:text-gray-800 mt-2  font-bold">
+            <div id="header-left" class="w-1/3 flex start flex items-center text-gray-500 mt-2  font-bold">
                  <a href="{{ route('mypagestu') }}">
                     @csrf
                     @method('POST')
@@ -39,50 +39,54 @@
     </nav>
 <!-- メイン -->
     <div class="mt-40 h-full text-center">
-  <form class="mb-6 mt-20" action="{{ route('presentation') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <input type="hidden" name="id" value="{{$id}}" />
-    <div class="flex flex-col items-center justify-center">
-  <div class="w-32 md:w-40 mb-4">
-    <img src="{{ asset('images/12920_paint.png') }}" alt="">
-  </div>
-  <h2 class="font-bold text-center text-gray-500 text-sm md:text-lg">
-    お世話になった先生へメッセージを送りましょう。</br>ぜひ写真も送って近況を知らせてください。
-  </h2>
-  <div class="w-32 md:w-40 mt-4">
-    <img src="{{ asset('images/12929_paint.png') }}" alt="">
-  </div>
-</div>
-
-    <div class="mx-auto max-w-2xl mt-10 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-      <label class="block mb-10">
-        <textarea class="px-4 py-4 resize-none w-full md:w-1/2 h-70 md:h-64 block rounded-md shadow-md focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-lg sm:text-sm sm:mx-auto sm:mt-10" name="message" id="" cols="30" rows="10" placeholder="メッセージを書いてください" type="text">{{ old('message') }}</textarea>
-      </label>
-      <form class="flex justify-center sm:mt-10">
-        <input type="file" accept='image/*' name="image" onchange="previewImage(this);">
-      </form>
-      <div class="flex justify-center">
-        <p>
-          <img id="preview" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-            style="max-width:200px;">
-        </p>
-      </div>
-      <x-input-error class="mb-4" :messages="$errors->all()"/>
-      <script>
-        function previewImage(obj) {
-          var fileReader = new FileReader();
-          fileReader.onload = (function () {
-            document.getElementById('preview').src = fileReader.result;
-          });
-          fileReader.readAsDataURL(obj.files[0]);
-        }
-      </script>
-      <div class="flex items-center justify-center mt-4">
-  <x-primary-button type="submit" class="shadow-lg bg-orange-500 hover:bg-orange-600 active:bg-orange-700 text-white rounded-full border-4 border-gray-300 px-4 py-2 w-50 h-12 font-bold">
-    <span class="text-xl">確 認</span>
-  </x-primary-button>
-</div>
-    </form>
-  </div>
+        <form class="mb-6 mt-20" action="{{ route('presentation') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <input type="hidden" name="id" value="{{$id}}" />
+            <div class="flex justify-center mt-10">
+                <div class="w-20 mr-3">
+                    <img src="{{ asset('images/12920_paint.png') }}" alt="">
+                </div>
+                <p class="font-bold mt-50 text-gray-500" style="font-size:24px;">
+                お世話になった先生へ<br>メッセージを送りましょう。
+            </p>
+                <div class="w-20 ml-3">
+                    <img src="{{ asset('images/12929_paint.png') }}" alt="">
+                </div>
+            </div>
+            <div class="mx-auto max-w-2xl mt-10  px-4  sm:px-6 lg:max-w-7xl lg:px-8">
+                <label class=" w-full h-full block flex justify-center mb-10">
+                    <textarea class="px-4 py-4 resize-none w-40 md:w-1/2 h-70 md:h-64 block rounded-md shadow-md focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="message" id="" cols="30" rows="10" placeholder="メッセージを書いてください" type="text" style="font-size:20px">{{ old('message') }}</textarea>
+                    <!-- <textarea class="w-30 h-30 mt-1 block rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" rows="3" placeholder="手紙を書いてください" type="text" name="message">{{ old('message') }}
+                        {{ old('message') }}
+                    </textarea> -->
+                </label>
+                <form class="mt-10">
+                    <input type="file" accept='image/*' name="image" onchange="previewImage(this);">
+                </form>
+                <div class="flex justify-center">
+                    <p>
+                        <!-- Preview:<br> -->
+                        <img id="preview" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+                            style="max-width:200px;">
+                    </p>
+                </div>
+                <x-input-error class="mb-4" :messages="$errors->all()"/>
+                <script>
+                    function previewImage(obj) {
+                        var fileReader = new FileReader();
+                        fileReader.onload = (function () {
+                            document.getElementById('preview').src = fileReader.result;
+                        });
+                        fileReader.readAsDataURL(obj.files[0]);
+                    }
+                </script>
+                <div class="flex items-center justify-center mt-4">
+                    <x-primary-button type="submit" class="shadow-lg bg-orange-500 shadow-orange-500/50 text-white rounded-full border-4 border-gray-300 px-4 py-2 text-xl w-50 h-12 font-bold mr-20">
+                        内容を確認する
+                    </x-primary-button>
+                </div>
+            </div>  
+        </form>
+    </div>
 </body>
 </html>
