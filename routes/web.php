05ec5bcb4f57aa53c7d38715a736728cc2ad6764
TaisukeMiyachi <taisuke.m.lotus.elise@gmail.com>
2023-04-06 20:18:29 +0900

    check

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::get('/', function () {
 
 //QRコード（ログインせずに見れるページ）
 Route::get('/messageqr/{id}', function ($id) {
-// dd($id);
+dd($id);
     $message = Message::find($id);
     $user_id = $message->user_id;
     $user = User::where('id', $user_id)->first();
