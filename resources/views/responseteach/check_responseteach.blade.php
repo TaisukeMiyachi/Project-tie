a2fe0a0e5bd3d00f78b511d146879fa8f8342ad8
TaisukeMiyachi <taisuke.m.lotus.elise@gmail.com>
2023-04-08 16:18:42 +0900

    check

--- resources/views/responseteach/check_responseteach.blade.php
+++ resources/views/responseteach/check_responseteach.blade.php
@@ -28,102 +28,52 @@
     }
 </style>
 
-<body class="bg-orange-50">
-    <!-- ヘッダー -->
-    <nav class="w-full bg-white shadow-lg">
-        <div class="flex items-center  h-24 max-w-7xl mx-auto px-4 sm:px-6 lg:px-">
-            <div id="header-left" class="w-1/3 flex start flex items-center text-gray-500 mt-2  font-bold">
-                 <a href="{{ route('mypageteach') }}">
-                    @csrf
-                    @method('POST')
-                    < 戻る</a>
-            </div>
-        </div>
-    </nav>
-    <section class="mt-40 h-full text-center">
-            <img src="{{ asset('images/BlueBird.png') }}" alt="PNG Image" width="200" height="200" style="margin: 30px auto;">
-            <!-- <h1 id="name" class="font-bold mt-0 mb-10 text-gray-500" style="font-size:48px;">Web Pigeon</h1> -->
-            <h1 id="name" class="font-bold mt-50 mb-0 text-gray-500" style="font-size:24px;">{{ $data->name }}さんへ</h1>
-            <div class="w-50 h-50 flex justify-center">
-            @if($data->image_name)
-                <div class="w-50 h-50 flex items-center justify-center">
-                    <img src="{{ asset('storage/images/'.$data->image_name)}}" class="mx-auto" style="height:150px; width:150px; object-fit: contain;" >
+    <body class="bg-orange-50">
+        <!-- ヘッダー -->
+        <nav class="w-full bg-white shadow-lg">
+            <div class="flex items-center  h-24 max-w-7xl mx-auto px-4 sm:px-6 lg:px-">
+                <div id="header-left" class="w-1/3 flex start flex items-center text-gray-500 mt-2  font-bold">
+                    <a href="{{ route('mypageteach') }}">
+                        @csrf
+                        @method('POST')
+                        < 戻る
+                    </a>
                 </div>
-            @else
-                <div class="bg-gray-300 flex items-center justify-center" style="height:150px; width:150px;">
-                    <span class="text-gray-500 text-lg md:text-xl" style="text-align: center; line-height: 150px;">noimage</span>
-                </div>
-            @endif
             </div>
-
-           <label class="w-full h-full block flex justify-center mb-10">
-                <textarea class="px-4 py-4 resize-none w-full sm:w-full md:w-1/2 h-70 md:h-64 block rounded-md shadow-md focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="message" type="text" style="font-size:20px">{{ $data->message }}</textarea>
-            </label>
-</section>
- <div class="flex items-center justify-center mt-4">
-            <form id="response-form" action="{{ route('responseteach.store') }}" method="POST">
-                @csrf
-                <input type="hidden" name="message" value="{{ $data->message }}">
-                <input type="hidden" name="image_name" value="{{ $data->image_name }}">                    
-                <input type="hidden" name="send_to" value="{{ $data->send_to }}">
-            
-                <button type="submit" class="ml-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                {{ __('送信') }}
-                </button>
-                </form>
-        </div>
-
-                <script>
-                function submitForms() {
-                    document.getElementById("response-form").submit();
-                }
-                </script>   
-        <!-- メイン -->
-        @csrf
-        <section class="flex justify-center">
-            <div class="w-full mt-60 sm:w-1/2 mb-10 md:w-1/3 p-2 md:p-3 lg:p-4 letter relative rounded-lg overflow-hidden shadow-lg">     
+        </nav>
+        <section class="mt-40 h-full text-center">
+                <img src="{{ asset('images/BlueBird.png') }}" alt="PNG Image" width="200" height="200" style="margin: 30px auto;">
+                    <h1 id="name" class="font-bold mt-50 mb-0 text-gray-500" style="font-size:24px;">{{ $data->name }}さんへ</h1>
                 <div class="w-50 h-50 flex justify-center">
                 @if($data->image_name)
-                <div class="w-40 h-40 flex items-center justify-center">
-                    <img src="{{ asset('storage/images/'.$data->image_name)}}" class="mx-auto" style="height:150px; width:150px; object-fit: contain;" >
-                </div>
+                    <div class="w-50 h-50 flex items-center justify-center">
+                        <img src="{{ asset('storage/images/'.$data->image_name)}}" class="mx-auto" style="height:150px; width:150px; object-fit: contain;" >
+                    </div>
                 @else
-                <div class="bg-gray-300 flex items-center justify-center" style="height:150px; width:150px;">
-                    <span class="text-gray-500 text-lg md:text-xl" style="text-align: center; line-height: 150px;">noimage</span>
-                </div>
+                    <div class="bg-gray-300 flex items-center justify-center" style="height:150px; width:150px;">
+                        <span class="text-gray-500 text-lg md:text-xl" style="text-align: center; line-height: 150px;">noimage</span>
+                    </div>
                 @endif
-            </div>
-            <div id="message" class="h-48 md:h-56 lg:h-64 bg-white bg-opacity-80 flex items-start justify-start p-4 md:p-6 lg:p-8" style="border-radius: 10px;">
-                <p id="message" class="bg-white px-4 py-4 w-full sm:w-full md:w-1/2 h-70 md:h-64 block rounded-md shadow-md focus:ring focus:ring-indigo-200 focus:ring-opacity-50 mx-auto">{{ $data->message }}</p>
-            </div>
+                </div>
+            <label class="w-full h-full block flex justify-center mb-10">
+                    <textarea class="px-4 py-4 resize-none w-full sm:w-full md:w-1/2 h-70 md:h-64 block rounded-md shadow-md focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="message" type="text" style="font-size:20px">{{ $data->message }}</textarea>
+                </label>
         </section>
-
-        
         <div class="flex items-center justify-center mt-4">
             <form id="response-form" action="{{ route('responseteach.store') }}" method="POST">
-                @csrf
+            @csrf
                 <input type="hidden" name="message" value="{{ $data->message }}">
                 <input type="hidden" name="image_name" value="{{ $data->image_name }}">                    
-                <input type="hidden" name="send_to" value="{{ $data->send_to }}">
-            
-                <button type="submit" class="ml-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                {{ __('送信') }}
-                </button>
-                </form>
+                <input type="hidden" name="send_to" value="{{ $data->send_to }}">            
+                    <button type="submit" class="ml-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    {{ __('送信') }}
+                    </button>
+            </form>
         </div>
-
-                <script>
-                function submitForms() {
-                    document.getElementById("response-form").submit();
-                }
-                </script>   
-</body>
+        <script>
+            function submitForms() {
+            document.getElementById("response-form").submit();
+            }
+        </script>      
+    </body>
 </html>
-
-
-
-
-
-
-
-
